32c83eb91d40947af7a3dfa869e55e94c7c6e529
Bruce <recall4056@gmail.com>
2024-05-22 01:22:26 +0800

    fix: async warning when bootstrap

--- config/index.ts
+++ config/index.ts
@@ -16,9 +16,15 @@ const config = {
     options: {}
   },
   framework: 'react',
-  compiler: 'webpack5',
+  // NOTE: https://github.com/NervJS/taro/issues/15413
+  compiler: {
+    prebundle: {
+      enable: false
+    },
+    type: 'webpack5'
+  },
   cache: {
-    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   mini: {
     postcss: {
