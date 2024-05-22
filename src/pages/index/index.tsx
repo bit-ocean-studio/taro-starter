922d4c6e87415b0589cd5aaa0bd019150d34e620
Bruce <recall4056@gmail.com>
2024-05-23 00:42:51 +0800

    refactor: convert class components to react hooks

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,40 +1,28 @@
-import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
 
 import 'taro-ui/dist/style/components/button.scss' // 按需引入
-import './index.scss'
 
-export default class Index extends Component<PropsWithChildren> {
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className="index">
-        <Text>Hello world!</Text>
-        <AtButton type="primary">I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton
-          type="primary"
-          circle
-        >
-          支持
-        </AtButton>
-        <Text>共建？</Text>
-        <AtButton
-          type="secondary"
-          circle
-        >
-          来
-        </AtButton>
-        环境变量：{process.env.TARO_APP_BASE_API_PREFIX}
-      </View>
-    )
-  }
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+      <AtButton type="primary">I need Taro UI</AtButton>
+      <Text>Taro UI 支持 Vue 了吗？</Text>
+      <AtButton
+        type="primary"
+        circle
+      >
+        支持
+      </AtButton>
+      <Text>共建？</Text>
+      <AtButton
+        type="secondary"
+        circle
+      >
+        来
+      </AtButton>
+      环境变量：{process.env.TARO_APP_BASE_API_PREFIX}
+    </View>
+  )
 }
